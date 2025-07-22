4bada5c52958db0ddd8198f4c8073ac3cc92068a
jadhavsagar123 <jadhavsagar2081997@gmail.com>
2025-07-22 22:51:31 +0530

    user created in convex

---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenerateProgramPage = () => {
+  return (
+    <div>
+      <h1>Generate Workout Program</h1>
+    </div>
+  )
+}
+
+export default GenerateProgramPage
